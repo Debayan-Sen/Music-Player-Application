36d7bd084b9cec6d3a87e8c388c85930e829d372
Debayan <sdebayan999@gmail.com>
2022-10-16 23:03:12 +0530

    separate resource file added

--- lib/player.dart
+++ lib/player.dart
@@ -1,5 +1,6 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:musicplayer/resources.dart';
 
 class Player extends StatefulWidget {
   const Player({Key? key}) : super(key: key);
@@ -10,71 +11,7 @@ class Player extends StatefulWidget {
 
 class _PlayerState extends State<Player> {
 
-  List Songs = [
-    {
-      "name": "Ami sudhu khujechi amay",
-      "thumbnail":"https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/4b/26/ef/4b26ef6a-bb5a-50ec-7e68-0baee4c1f83b/5059074614465.jpg/375x375bb.jpg",
-      "path":"songs/Ami_sudhu_khujechi_amay.mp3",
-      "artist":"Taalpatar Sepai",
-      "genre":"Indian Film Pop"
-    },
-    {
-      "name": "Jogi",
-      "thumbnail":"https://i.scdn.co/image/ab67616d0000b27316f7c961e44c0116cd736db6",
-      "path":"songs/Jogi.mp3",
-      "artist":"Akanksha Sharma and Yasser Desai",
-      "genre":"Romance"
-    },
-    {
-      "name": "Kesariya",
-      "thumbnail":"https://akm-img-a-in.tosshub.com/aajtak/images/story/202207/kesariya_song_alia_bhatt_ranbir_kapoor-sixteen_nine.png",
-      "path":"songs/Kesariya.mp3",
-      "artist":"Arijit Singh, Amitabh Bhattacharya, Pritam Chakraborty",
-      "genre":"Romance"
-    },
-    {
-      "name": "Kun Faya Kun",
-      "thumbnail":"https://i.scdn.co/image/ab67616d0000b27354e544672baa16145d67612b",
-      "path":"songs/Kun_Faya_Kun.mp3",
-      "artist":"A. R. Rahman, Javed Ali, and Mohit Chauhan",
-      "genre":"Sufi"
-    },
-    {
-      "name": "Main Agar Kahoon",
-      "thumbnail":"https://images.genius.com/cd6e2060b1ec76cc087edde972ef8e28.1000x750x1.jpg",
-      "path":"songs/Main_Agar_Kahoon.mp3",
-      "artist":"Shreya Ghoshal and Sonu Nigam",
-      "genre":"Romance"
-    },
-    {
-      "name": "Mera Mann Kehne Laga",
-      "thumbnail":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuxZ8ZDtNdtN3sF9QP-biYSBNt3H93ep-d3Q&usqp=CAU",
-      "path":"songs/Mera_Mann_Kehne_Laga.mp3",
-      "artist":"Falak Shabir",
-      "genre":"Pop"
-    },
-    {
-      "name": "Moddhorate",
-      "thumbnail":"https://i.scdn.co/image/ab67616d0000b27319f7a13ec1d7988194e651ad",
-      "path":"songs/Moddhorate.mp3",
-      "artist":"Taalpatar Sepai",
-      "genre":"Indian Film Pop"
-    },
-    {
-      "name": "Sunflower",
-      "thumbnail":"https://www.pluggedin.com/wp-content/uploads/2020/01/Post_Malone__Sunflower__Large-1024x587.jpg",
-      "path":"songs/Sunflower.mp3",
-      "artist":"Post Malone, Swae Lee",
-      "genre":"Hip-Hop"
-    },
-    {
-      "name": "The Nights",
-      "thumbnail":"https://i.scdn.co/image/ab67616d0000b2730ae4f4d42e4a09f3a29f64ad",
-      "path":"songs/The_Nights.mp3",
-      "artist":"Avicii",
-      "genre":"Electronic dance music"
-    },
-  ];
+  List Songs = Resources.Songs;
 
   final audioPlayer = AudioPlayer();
   bool isPlaying = false;
